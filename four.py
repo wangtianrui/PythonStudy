35828704a3676d3f4b93f7e355f5ad4eb95d5daa
Your Name <1056992492@qq.com>
2017-10-15 11:51:31 +0800

    1

--- four.py
+++ four.py
@@ -9,7 +9,6 @@ class Dog():
         self.name = name
         self.age = age
     '''
-
     def __init__(self, name):
         self.name = name
         self.love = 'chen'  # 设定默认值
@@ -17,23 +16,18 @@ class Dog():
     def sit(self):
         print(self.name + " is sitting.")
 
-
-my_dog = Dog("wang")
-my_dog.sit()
-my_dog.name = "wang tian"
-my_dog.sit()
-
-
 class EattingDog(Dog):
-    def __int__(self, name):
+    def __init__(self, name):
         super().__init__(name)
         self.age = 10
 
-
     def printf(self):
         print(self.name + " is eatting")
-
+    def sit(self):
+        print("eatting dog is sitting")
 
 eatting_dog = EattingDog("wang")
 eatting_dog.printf()
+print(eatting_dog.age)
 print(eatting_dog.love)
+eatting_dog.sit()
